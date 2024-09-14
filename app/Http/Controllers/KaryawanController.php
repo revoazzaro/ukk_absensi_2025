c5015506c377df3980bd28a09a895e540d91fa65
Revo Sasta Azzaro <revosastaazzaro16@gmail.com>
2024-09-14 21:53:23 +0700

    add: edit profile

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Karyawan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class KaryawanController extends Controller
 {
@@ -38,8 +41,33 @@ class KaryawanController extends Controller
         return view('pages.profile'); 
     }
 
-    public function edit_profile (Request $request)
-    {}
+    public function update_profile (Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|max:255',
+            'email' => 'required|email:rfc,dns|max:255',
+            'foto_profil' => 'image|mimes:png,jpg,jpeg'
+        ]);
+
+        $data = $request->only('nama', 'email');
+        $same_email = Karyawan::where('email', $request->email)->whereNot('id', Auth::user()->id)->exists();
+        if($same_email) return redirect()->back()->withInput()->withErrors(['email' => 'Email already registered!']);
+
+        if($request->foto_profil) {
+            $file = $request->file('foto_profil');
+            $file_path = $file->store('foto_profil', 'public');
+            if(!$file_path) return redirect()->back()->withInput()->withErrors(['foto_profil' => 'Foto profil failed to upload!']);
+
+            if(Auth::user()->foto_profil != 'foto_profil/default.png') {
+                Storage::delete(Auth::user()->foto_profil);
+            }
+            $data['foto_profil'] = $file_path;
+        }
+
+        $update = Karyawan::find(Auth::user()->id)->update($data);
+        if($update) return redirect()->route('profile');
+        return redirect()->back()->withInput()->withErrors(['nama' => 'Failed to update profile!']);
+    }
 
     /**
      * Show the form for editing the specified resource.
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,5 +1,44 @@
 @extends('layout.app')
 @section('title', 'Profile')
 @section('content')
-
+<ul class="list-group col-10">
+    <form action="{{ route('profile') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <li class="list-group-item">
+            <label for="foto_profil" style="width: 15rem; height: 15rem;" class="mx-auto d-block">
+                <img src="{{ asset('storage/' . auth()->user()->foto_profil) }}" width="100%" height="100%" alt="Profile Image" class="mx-auto d-block rounded-circle">
+            </label>
+        </li>
+        <li class="list-group-item">
+            <div class="row justify-space-between align-items-center">
+                <label for="nama" class="col-12 col-lg-2">Nama:</label>
+                <input type="text" id="nama" name="nama" class="form-control col-12 col-lg-10" value="{{ old('nama') ?? auth()->user()->nama }}">
+            </div>
+            @error('nama')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </li>
+        <li class="list-group-item">
+            <div class="row justify-space-between align-items-center">
+                <label for="email" class="col-12 col-lg-2">Email:</label>
+                <input type="email" id="email" name="email" class="form-control col-12 col-lg-10" value="{{ old('email') ?? auth()->user()->email }}">
+            </div>
+            @error('email')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </li>
+        <li class="list-group-item">
+            <div class="row justify-space-between align-items-center">
+                <label for="foto_profil" class="col-12 col-lg-2">Foto Profil:</label>
+                <input type="file" id="foto_profil" name="foto_profil" class="form-control col-12 col-lg-10">
+            </div>
+            @error('foto_profil')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </li>
+        <li class="list-group-item">
+            <button type="submit" class="btn btn-warning mx-auto d-block col-10 col-lg-8">EDIT</button>
+        </li>
+    </form>
+</ul>
 @endsection
